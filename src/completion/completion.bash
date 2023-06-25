e612f6070fd65b2b041b5d7943d619aeb0baa979
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2023-06-25 20:07:40 +0200

    add completion script

---
+++ src/completion/completion.bash
@@ -0,0 +1,18 @@
+# shellcheck disable=SC2155,SC2207
+
+_toolbox_completions() {
+    case "${#COMP_WORDS[@]}" in
+        1)
+            return 0
+            ;;
+        2)
+            local COMMANDS=$(toolbox --commands)
+            COMPREPLY=($(compgen -W "${COMMANDS}" "${COMP_WORDS[1]}"))
+            ;;
+        *)
+            # TODO: take suggestions from corresponding command
+            ;;
+    esac
+}
+
+complete -F _toolbox_completions toolbox

1d9a0e9cac039dfa6b3da5db63b0ac2620e9085f
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2023-07-04 21:05:56 +0200

    add base64 command

---
+++ src/command/base64.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# shellcheck disable=SC2155
+
+function main() {
+    case "${1}" in
+        "--toolbox-description")
+            command_description
+            ;;
+        "--toolbox-completion")
+            command_completion "${@:2}"
+            ;;
+        "-h"|"--help")
+            command_help "${@:2}"
+            ;;
+        *)
+            command_execute "$@"
+            ;;
+    esac
+}
+
+function command_description() {
+    echo "Decode or encode base64 input"
+}
+
+function command_completion() {
+    case "${#@}" in
+        1)
+            echo "decode encode"
+            ;;
+    esac
+}
+
+function command_execute() {
+    case "${1}" in
+        "decode")
+            echo -n "${*:2}" | base64 --decode --ignore-garbage
+            echo
+            ;;
+        "encode")
+            echo -n "${*:2}" | base64 --wrap=0
+            echo
+            ;;
+        "")
+            echo -e "Action is required. See \e[32m--help\e[0m for available arguments"
+            exit 1
+            ;;
+        *)
+            echo -e "Unexpected action \e[33m${1}\e[0m. See \e[32m--help\e[0m for available arguments"
+            exit 1
+            ;;
+    esac
+}
+
+function command_help() {
+    local TEXT=$(cat << HEREDOC
+\e[33mDescription:\e[0m
+  $(command_description)
+
+\e[33mUsage:\e[0m
+  base64 [options] <action> <data>
+  base64 encode string-to-encode
+
+\e[33mArguments:\e[0m
+  \e[32maction\e[0m      Action to perform. Available values: "decode", "encode".
+  \e[32mdata\e[0m        String data to decode or encode
+
+\e[33mOptions:\e[0m
+  \e[32m-h, --help\e[0m  Display this help
+HEREDOC
+)
+    echo -e "${TEXT}"
+}
+
+main "$@"

56d95772fc46ff8fb50f680070b87397798bfede
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2023-07-19 21:34:22 +0200

    add verbose option to lint command

--- src/command/lint.sh
+++ src/command/lint.sh
@@ -25,8 +25,8 @@ function command_description() {
 function command_completion() {
     case "${#@}" in
         1)
-            [[ ${1} = "--"* ]] && echo "--help" && exit
-            [[ ${1} = "-"* ]] && echo "-h" && exit
+            [[ ${1} = "--"* ]] && echo "--help --verbose" && exit
+            [[ ${1} = "-"* ]] && echo "-h -v" && exit
 
             echo ""
             ;;
@@ -39,20 +39,20 @@ function command_execute() {
     shellcheck --version
     echo
 
+    local IS_VERBOSE=false
+    if [ "${1}" == "-v" ] || [ "${1}" == "--verbose" ]; then
+        IS_VERBOSE=true
+        shift
+    fi
+
     if [ -n "${1}" ]; then
         local FILES=$(get_files_from_arguments "${@}")
     else
         local FILES=$(get_project_scripts)
     fi
 
-    local FILES_COUNT="$(echo "${FILES}" | wc -l)"
-    if [ "${FILES_COUNT}" == 1 ]; then
-        echo -e "Checking \e[36m${FILES}\e[0m..." && echo
-    else
-        echo -e "Checking \e[36m${FILES_COUNT}\e[0m files..." && echo
-    fi
-
-    run_shellcheck "${FILES[@]}" && echo -e "\e[42m OK \e[0m"
+    print_files_to_process "${FILES}" "${IS_VERBOSE}"
+    run_shellcheck "${FILES[@]}" && echo -e "\n\e[42m OK \e[0m"
 }
 
 function get_project_root_dir() {
@@ -87,6 +87,27 @@ function get_project_scripts() {
         | xargs -0 grep -lE '^#!.*/(sh|bash)'
 }
 
+function print_files_to_process() {
+    local FILES=()
+    readarray -t FILES <<< "${1}"
+    local IS_VERBOSE=${2}
+
+    if [ "${#FILES[@]}" == 1 ]; then
+        local FILE="$(echo "${FILES[0]#"${DIR_PROJECT_ROOT}"}" | sed -E "s/^\///g")"
+        echo -e "Checking \e[36m${FILE}\e[0m..."
+    else
+        echo -e "Checking \e[36m${#FILES[@]}\e[0m files..."
+
+        if [ "${IS_VERBOSE}" == true ]; then
+            echo
+            for FILE in "${FILES[@]}"; do
+                echo -n "  "
+                echo "${FILE#"${DIR_PROJECT_ROOT}"}" | sed -E "s/^\///g"
+            done
+        fi
+    fi
+}
+
 # https://github.com/koalaman/shellcheck/wiki/Recursiveness
 function run_shellcheck() {
     echo "${@}" | xargs shellcheck --color=always --severity=info
@@ -99,13 +120,14 @@ function command_help() {
 
 \e[33mUsage:\e[0m
   lint [options] [<files>...]
-  lint src/command/anycommand.sh
+  lint src/command/anycommand.sh src/command/othercommand.sh
 
 \e[33mArguments:\e[0m
-  \e[32mfiles\e[0m       Specific files to process. If ommited all the project shell scripts will be processed.
+  \e[32mfiles\e[0m          Specific files to process. If ommited all the project shell scripts will be processed.
 
 \e[33mOptions:\e[0m
-  \e[32m-h, --help\e[0m  Display this help
+  \e[32m-h, --help\e[0m     Display this help
+  \e[32m-v, --verbose\e[0m  Print files that will be processed
 HEREDOC
 )
     echo -e "${TEXT}"
